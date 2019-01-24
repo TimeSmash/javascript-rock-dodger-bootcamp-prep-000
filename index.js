ec704f422437d68957592e7e8b5bcf438c1c5d28
mcumm64 <mcumm64@gmail.com>
2019-01-24 23:29:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,22 +18,22 @@ var gameInterval = null
  */
 
 function checkCollision(rock) {
-  const top = positionToInteger(rock.style.top)
-
-  // rocks are 20px high
-  // DODGER is 20px high
-  // GAME_HEIGHT - 20 - 20 = 360px;
+    const top = positionToInteger(rock.style.top);
+// GAME_HEIGHT - 20 - 20 = 360px;
   if (top > 360) {
+ 
     const dodgerLeftEdge = positionToInteger(DODGER.style.left)
-    const dodgerRightEdge = dodgerLeftEdge + 40;
+    const dodgerRightEdge = positionToInteger((DODGER.style.left) + 40)
     const rockLeftEdge = positionToInteger(rock.style.left)
-    const rockRightEdge = rockLeftEdge + 20;
+    const rockRightEdge = positionToInteger((rock.style.left) + 20);
 
-    return (
-      (rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge) ||
-      (rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge) ||
-      (rockLeftEdge <= dodgerRightEdge && rockRightEdge >= dodgerRightEdge)
-    )
+    let collide1 = ((rockLeftEdge <= dodgerLeftEdge) && (rockRightEdge >= dodgerLeftEdge));
+    let collide2 = ((rockLeftEdge >= dodgerLeftEdge) && (rockRightEdge <= dodgerRightEdge));
+    let collide3 = ((rockLeftEdge <= dodgerRightEdge) && (rockRightEdge >= dodgerRightEdge));
+    
+    if ( collide1 || collide2 || collide3) {
+      return true;
+    }
   }
 }
 
